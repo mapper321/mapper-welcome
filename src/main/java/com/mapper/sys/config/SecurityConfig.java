a7a740329b8aa943032f0faff24340fcf4347e1c
mapper321 <547008022@qq.com>
2018-10-16 22:49:21 +0800

    暂时去掉安全校验 修改swagger，在生产环境下要改配置eg: springfox:   documentation:     swagger:       v2:         host: daily.mapper321.cn

--- src/main/java/com/mapper/sys/config/SecurityConfig.java
+++ src/main/java/com/mapper/sys/config/SecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-@Configuration
+//@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
